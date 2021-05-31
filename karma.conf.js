f6675120e87646b955311bf0dfe22798bed53a7b
ellurukalyan <ellurukalyan@gmail.com>
2021-05-31 15:15:24 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const process = require('process');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -22,6 +24,12 @@ module.exports = function (config) {
     },
     angularCli: {
       environment: 'dev'
+    },
+    {
+  //...
+  browsers: ['ChromeHeadless']
+
+  //...
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
